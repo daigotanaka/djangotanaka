de022f23844351072385cb9e559b54f02d1f83f9
daigotanaka <daigo.tanaka@gmail.com>
2014-04-23 22:32:09 -0700

    Added created_by to Page

--- api/resources.py
+++ api/resources.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from tastypie.authentication import Authentication, ApiKeyAuthentication
-from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie import fields
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.authorization import Authorization
 from tastypie.exceptions import Unauthorized
 from tastypie.models import create_api_key
-from tastypie.resources import ALL, ModelResource
+from tastypie.resources import ALL, ALL_WITH_RELATIONS, ModelResource
 
 from core.models import Page
 
@@ -19,7 +20,29 @@ class SingleItemModificationAuthorization(Authorization):
     def delete_list(self, object_list, bundle):
         raise Unauthorized("Sorry, no bulk delete.")
 
+    def read_list(self, object_list, bundle):
+        return object_list.filter(created_by=bundle.request.user)
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        allowed_methods = []
+        excludes = [
+            "email",
+            "password",
+            "is_active",
+            "is_staff",
+            "is_superuser"
+        ]
+        filtering = {
+            "username": ALL
+        }
+
+
 class PageResource(ModelResource):
+    created_by = fields.ForeignKey(UserResource, "created_by")
+
     class Meta:
         queryset = Page.objects.all()
         allowed_methods = ["post", "get", "patch", "delete"]
@@ -28,8 +51,13 @@ class PageResource(ModelResource):
         excludes = ["id"]
         detail_uri_name = "slug"
         filtering = {
+            "created_by": ALL_WITH_RELATIONS,
             "slug": ALL,
             "title": ALL,
             "status": ALL,
             "body": ALL
         }
+
+    def obj_create(self, bundle, **kwargs):
+        return super(PageResource, self).obj_create(
+            bundle, created_by=bundle.request.user)
--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.contrib.auth.models import User
 from django.db.models import Model
-from django.db.models import CharField, DateTimeField, TextField
+from django.db.models import CharField, DateTimeField, ForeignKey, TextField
 
 
 STATUS_CHOICES = (
@@ -10,10 +10,13 @@ STATUS_CHOICES = (
     ("public", "public")
 )
 
+
 class Page(Model):
+    created_by = ForeignKey(User, related_name="created_pages")
     created_at = DateTimeField()
     modified_at = DateTimeField(editable=False)
-    status = CharField(max_length=16, choices=STATUS_CHOICES, default="private")
+    status = CharField(
+        max_length=16, choices=STATUS_CHOICES, default="private")
     title = CharField(blank=True, max_length=256)
     slug = CharField(blank=False, unique=True, max_length=256)
     head = TextField(blank=True)
