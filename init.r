d8faaa4c1905ddeced10f4c6050ce2261bb6e246
daigotanaka <daigo.tanaka@gmail.com>
2014-11-23 01:59:55 -0800

    Added init.r

---
+++ init.r
@@ -0,0 +1,4 @@
+install.packages("datasets", dependencies = TRUE)
+install.packages("methods", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("knitr", dependencies = TRUE)
