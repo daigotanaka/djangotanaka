709b9e57d8ea664715fd9bb89729f99324c3e0c2
daigotanaka <daigo.tanaka@gmail.com>
2014-11-24 14:48:41 -0800

    Delete cache first before setting new value

--- libs/utils.py
+++ libs/utils.py
@@ -1,7 +1,8 @@
 from django.core.cache import cache
 
-#get the cache key for storage
+
 def cache_get_key(*args, **kwargs):
+    """Get the cache key for storage"""
     import hashlib
     serialise = []
     for arg in args:
@@ -14,13 +15,15 @@ def cache_get_key(*args, **kwargs):
     key = hashlib.md5("".join(serialise)).hexdigest()
     return key
 
-#decorator for caching functions
+
 def cache_for(time):
+    """Decorator for caching functions"""
     def decorator(fn):
         def wrapper(*args, **kwargs):
             key = cache_get_key(fn.__name__, *args, **kwargs)
             result = cache.get(key)
             if not result or "clear_cache" in kwargs and kwargs["clear_cache"]:
+                cache.delete(key)
                 result = fn(*args, **kwargs)
                 cache.set(key, result, time)
             return result
